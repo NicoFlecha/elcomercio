00a560df84696303d27f0e751eca5ba72974e135
Nico Flecha <nflecha@sysworld.com.ar>
2021-07-15 22:22:38 -0300

    primer commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Este es un ecommerce
         </p>
         <a
           className="App-link"
